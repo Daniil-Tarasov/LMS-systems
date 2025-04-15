95d36539eb415dcde29939405aab7e0df6928c56
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-04-15 21:16:05 +0300

    Sml fxs

--- materials/models.py
+++ materials/models.py
@@ -22,7 +22,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(
         Course, on_delete=models.CASCADE, verbose_name='Курс', related_name='lesson', null=True, blank=True
     )
-    link_to_video = models.TextField(verbose_name='Ссылка на видео')
+    link_to_video = models.URLField(verbose_name='Ссылка на видео')
 
     def __str__(self):
         return f'{self.title_lesson}'
