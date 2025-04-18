85da625c43385783cb806a493f2c8339a1556f40
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-04-18 16:49:13 +0300

    Task 1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,29 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+
     class Meta:
         model = Course
         fields = '__all__'
 
 
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons = SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ('title_course', 'description', 'count_lessons')
+
+    def get_count_lessons(self, course):
+        return course.lesson.count()
+
+
 class LessonSerializer(ModelSerializer):
+
     class Meta:
         model = Lesson
         fields = '__all__'
